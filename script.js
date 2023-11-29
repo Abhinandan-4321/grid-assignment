b1cfc94e70ea0ab5f8ff45cc88c863827264248d
Abhinandan-4321 <abhinandangupta1105@gmail.com>
2023-11-29 17:45:55 +0530

    First

---
+++ script.js
@@ -0,0 +1,11 @@
+// const popup = document.getElementById('popup')
+// popup.style.display = 'none'
+// var doit = () =>{
+//    popup.style.display = ''// we use style here to tell that it goes to CSS
+// }
+
+// window.addEventListener('keydown', function(event){
+//     if(event.key==='Escape'){
+//         popup.style.display = "none"
+//     }
+// })
